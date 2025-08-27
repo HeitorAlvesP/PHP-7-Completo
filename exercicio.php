c1b930dd1d918daa134e601715aaccd1eae1f54a
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2025-08-27 10:16:22 -0300

    styles da navegação

--- exercicio.php
+++ exercicio.php
@@ -4,14 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php modulos</title>
+
     <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/exercicio.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    
 </head>
-<body>
+<body class="exercicio">
 
     <header class="cabecalho">
         <h1>Curso PHP</h1>
         <h2>Vizualização do exercicio</h2>
     </header>
+
+    <nav class="navegacao">
+        <a href="" class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
 
     <main class="principal">
         <div class="conteudo">
